011982010500a238be05afbcd1d04f28b3fc0f14
Han Soo Kwan <80747187+HanSooKwan@users.noreply.github.com>
2021-12-01 08:19:37 +0900

    Update AdaptiveWarpingLayer_Check.py

--- AdaptiveWarpingLayer_Check.py
+++ AdaptiveWarpingLayer_Check.py
@@ -15,13 +15,14 @@ if device == "cuda":
 else:
     torch.set_default_tensor_type(torch.FloatTensor)
 batch = 4
+CH = 99
 H = 4
 W = 8
 
 # Random Input tensors (for gradcheck)
 # image = torch.randn(batch,3,256,448,dtype=torch.float32, requires_grad=True)
 depth = torch.randn(batch,1,256,448,dtype=torch.float32, requires_grad=True) + 2.0
-context = torch.randn(batch, 195, 256, 448, dtype=torch.float32, requires_grad=True)
+context = torch.randn(batch, 99, 256, 448, dtype=torch.float32, requires_grad=True)
 kernel = torch.randn(batch,16,256,448,dtype=torch.float32,requires_grad=True)
 flow = torch.randn(batch,2,256,448,dtype=torch.float32,requires_grad=True)
 
