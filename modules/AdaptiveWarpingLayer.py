60e8b648ff99d57b8594ecb8fa7f6e230387944a
Han Soo Kwan <80747187+HanSooKwan@users.noreply.github.com>
2021-12-01 08:46:46 +0900

    Update AdaptiveWarpingLayer.py

--- modules/AdaptiveWarpingLayer.py
+++ modules/AdaptiveWarpingLayer.py
@@ -38,7 +38,7 @@ class AdaptiveWarpingLayer_Function(Function):
             dim=-1)  # Shape: B, 1, 1, 1
         channel_mesh = torch.arange(start=0, end=Ch).unsqueeze(dim=0).unsqueeze(dim=-1).unsqueeze(
             dim=-1)  # Shape: 1, Ch, 1, 1
-        mesh_of_tp1 = torch.stack([x_grid, y_grid], dim=0).unsqueeze(dim=0)
+        mesh = torch.stack([x_grid, y_grid], dim=0).unsqueeze(dim=0)
 
         # For I(t+1) image, where would the corresponding pixels in I(t) be?? (Estimate using flow from I(t+1)->I(t) IF is_backward is True, ELSE use I(t)->I(t+1) with missing holes) [Getting x+[f(x)]]
 
@@ -47,17 +47,17 @@ class AdaptiveWarpingLayer_Function(Function):
                 r = torch.Tensor([[-1, -1, -1, -1, 0, 0, 0, 0, 1, 1, 1, 1, 2, 2, 2, 2],
                                   [-1, 0, 1, 2, -1, 0, 1, 2, -1, 0, 1, 2, -1, 0, 1, 2]]).type(
                     torch.cuda.LongTensor).T.unsqueeze(dim=1).unsqueeze(dim=-1).unsqueeze(dim=-1)
-                mesh_of_t = torch.floor(mesh_of_tp1 + flow).type(torch.cuda.LongTensor)  # Shape: B, 2, H, W
+                mesh = torch.floor(mesh + flow).type(torch.cuda.LongTensor)  # Shape: B, 2, H, W
                 theta = flow - torch.floor(flow)  # Fractional part, Shape: B, 2, H, W
-                mesh_of_t_16 = mesh_of_t.repeat(16, 1, 1, 1, 1) + r  # Shape: 16, B, 2, H, W
+                mesh = mesh.repeat(16, 1, 1, 1, 1) + r  # Shape: 16, B, 2, H, W
 
             elif device == "cpu":
                 r = torch.Tensor([[-1, -1, -1, -1, 0, 0, 0, 0, 1, 1, 1, 1, 2, 2, 2, 2],
                                   [-1, 0, 1, 2, -1, 0, 1, 2, -1, 0, 1, 2, -1, 0, 1, 2]]).type(
                     torch.LongTensor).T.unsqueeze(dim=1).unsqueeze(dim=-1).unsqueeze(dim=-1)
-                mesh_of_t = torch.floor(mesh_of_tp1 + flow).type(torch.LongTensor)  # Shape: B, 2, H, W
+                mesh = torch.floor(mesh + flow).type(torch.LongTensor)  # Shape: B, 2, H, W
                 theta = flow - torch.floor(flow)  # Fractional part, Shape: B, 2, H, W
-                mesh_of_t_16 = mesh_of_t.repeat(16, 1, 1, 1, 1) + r  # Shape: 16, B, 2, H, W
+                mesh = mesh.repeat(16, 1, 1, 1, 1) + r  # Shape: 16, B, 2, H, W
 
         # else: # Use forward flow (I(t))->I(t+1))
         #     if device == "cuda":
@@ -66,20 +66,23 @@ class AdaptiveWarpingLayer_Function(Function):
         #         mesh_of_tp1 = torch.floor(mesh_of_tp1 - flow).type(torch.LongTensor)
 
         # Boolean array checking if in_range (shape: 4, B, H, W)
-        in_range = torch.stack([mesh_of_t_16[:, :, 0] >= 0, mesh_of_t_16[:, :, 0] < W, mesh_of_t_16[:, :, 1] >= 0,
-                                mesh_of_t_16[:, :, 1] < H], dim=0)  # Shape: 4, 16, B, H, W
+        in_range = torch.stack([mesh[:, :, 0] >= 0, mesh[:, :, 0] < W, mesh[:, :, 1] >= 0, mesh[:, :, 1] < H],
+                               dim=0)  # Shape: 4, 16, B, H, W
         in_range = torch.all(in_range, dim=0)  # Shape: 16, B, H, W
 
         # Only consider "future is in-range" pixels
-        mesh_of_t_16 = in_range.unsqueeze(dim=2) * mesh_of_t_16  # Shape: 16, B, 2, H, W
+        mesh = in_range.unsqueeze(dim=2) * mesh  # Shape: 16, B, 2, H, W
 
         # Indices (y, x)
-        y_of_t_16 = mesh_of_t_16[:, :, 1].unsqueeze(dim=2)
-        x_of_t_16 = mesh_of_t_16[:, :, 0].unsqueeze(dim=2)
+        # y_of_t_16 =
+        # x_of_t_16 =
 
         # Correspondence in I(t) of pixels in I(t+1) (for image, depth, context)
-        image_picked_16 = image[batch_mesh.unsqueeze(dim=0), channel_mesh.unsqueeze(dim=0), y_of_t_16, x_of_t_16]
-        image_picked_16 = in_range.unsqueeze(dim=2) * image_picked_16  # Set image pixel values to 0 if out of range
+        image = image[
+            batch_mesh.unsqueeze(dim=0), channel_mesh.unsqueeze(dim=0), mesh[:, :, 1].unsqueeze(dim=2), mesh[:, :,
+                                                                                                        0].unsqueeze(
+                dim=2)]
+        image = in_range.unsqueeze(dim=2) * image  # Set image pixel values to 0 if out of range
         # depth_picked_16 = depth[batch_mesh.unsqueeze(dim=0),channel_mesh_depth.unsqueeze(dim=0),y_of_t_16, x_of_t_16]
         # depth_picked_16 = in_range.unsqueeze(dim=2) * depth_picked_16
 
@@ -87,19 +90,21 @@ class AdaptiveWarpingLayer_Function(Function):
         kernel = kernel.permute(1, 0, 2, 3).unsqueeze(dim=2)  # Shape: 16, B, 1, H, W
 
         # Bilinear interpolation coefficients
-        theta_u = theta[:, 0].unsqueeze(dim=1)  # Shape: B,1,H,W
-        theta_v = theta[:, 1].unsqueeze(dim=1)  # Shape: B,1,H,W
-        theta_16 = torch.stack([1 - theta_u] * 8 + [theta_u] * 8, dim=0) * torch.stack(
-            [1 - theta_v, 1 - theta_v, theta_v, theta_v] * 4, dim=0)  # Shape: 16, B, 1, H, W
+        # theta_u = theta[:,0].unsqueeze(dim=1) # Shape: B,1,H,W
+        # theta_v = theta[:,1].unsqueeze(dim=1) # Shape: B,1,H,W
+        theta_16 = torch.stack([1 - theta[:, 0].unsqueeze(dim=1)] * 8 + [theta[:, 0].unsqueeze(dim=1)] * 8, dim=0) \
+                   * torch.stack(
+            [1 - theta[:, 1].unsqueeze(dim=1), 1 - theta[:, 1].unsqueeze(dim=1), theta[:, 1].unsqueeze(dim=1),
+             theta[:, 1].unsqueeze(dim=1)] * 4, dim=0)  # Shape: 16, B, 1, H, W
 
         # Multiply all
-        image_warped_16 = image_picked_16 * kernel * theta_16  # shape: 16, B, Ch, H, W
+        image_warped = image * kernel * theta_16  # shape: 16, B, Ch, H, W
 
         # Sum all for dim=0 (Sum for 16 corresponding pixels)
-        image_warped = torch.sum(image_warped_16, dim=0)
+        image_warped = torch.sum(image_warped, dim=0)
 
         # Save tensors for backward
-        ctx.save_for_backward(image_picked_16, mesh_of_t_16, in_range, kernel, theta_16, theta_u, theta_v)
+        ctx.save_for_backward(image, mesh, in_range, kernel, theta_16, theta)
 
         return image_warped
 
@@ -110,8 +115,8 @@ class AdaptiveWarpingLayer_Function(Function):
         grad_image = grad_kernel = grad_flow = None
 
         # Retrieve saved tensors from forward
-        image_picked_16, mesh_of_t_16, in_range, kernel, theta_16, theta_u, theta_v = ctx.saved_tensors
-        # RECALL: image_picked_16 --> Image values of In-range 16 correspondences in I(t) of pixel in I(t+1) / Shape: 16, B, Ch, H, W
+        image, mesh_of_t_16, in_range, kernel, theta_16, theta = ctx.saved_tensors
+        # RECALL: image --> Image values of In-range 16 correspondences in I(t) of pixel in I(t+1) / Shape: 16, B, Ch, H, W
         # RECALL: mesh_of_t_16--> Coordinates of In-range 16 correspondences in I(t) of pixel in I(t+1) / Shape: 16, B, 2, H, W
         # RECALL: in_range--> Boolean tensor of checking if 16 correspondences (of that pixel) are in-range of image / Shape: 16, B, H, W
         # RECALL: kernel --> 16 interpolation coefficients (of that pixel) / # Shape: 16, B, 1, H, W
@@ -119,7 +124,7 @@ class AdaptiveWarpingLayer_Function(Function):
         # RECALL: theta_u, theta_v --> Fractional parts of flow from I(t+1)->I(t) / Shape: B, 1, H, W
 
         # Get shape
-        _, B, Ch, H, W = image_picked_16.shape
+        _, B, Ch, H, W = image.shape
 
         if ctx.needs_input_grad[0]:
             # Ravel (Warning: DO NOT ADD torch.arange(0,16) since we are going to add it in first place, NOT sum it by dimension after unraveling)
@@ -197,22 +202,27 @@ class AdaptiveWarpingLayer_Function(Function):
             #     grad_image = torch.reshape(grad_image, [B,1,H,W])
 
         if ctx.needs_input_grad[1]:
-            grad_kernel = torch.sum(image_picked_16 * theta_16 * grad_image_warped.unsqueeze(dim=0),
-                                    dim=2)  # Shape: 16, B, H, W
+            grad_kernel = torch.sum(image * theta_16 * grad_image_warped.unsqueeze(dim=0), dim=2)  # Shape: 16, B, H, W
             grad_kernel = grad_kernel.permute(1, 0, 2, 3)  # Reshape for output
 
         if ctx.needs_input_grad[2]:
             # Get gradient of image pixel value wrt flow: u-direction
-            grad_bilinearcoef_flow_u = torch.stack([-1 + theta_v, -1 + theta_v, -theta_v, -theta_v] * 2 +
-                                                   [1 - theta_v, 1 - theta_v, theta_v, theta_v] * 2
-                                                   , dim=0)  # Shape: 16 * B * 1 * H * W
-            grad_flow_u = kernel * image_picked_16 * grad_bilinearcoef_flow_u  # Shape: 16, B, Ch, H, W
+            grad_bilinearcoef_flow_u = torch.stack(
+                [-1 + theta[:, 1].unsqueeze(dim=1), -1 + theta[:, 1].unsqueeze(dim=1), -theta[:, 1].unsqueeze(dim=1),
+                 -theta[:, 1].unsqueeze(dim=1)] * 2 +
+                [1 - theta[:, 1].unsqueeze(dim=1), 1 - theta[:, 1].unsqueeze(dim=1), theta[:, 1].unsqueeze(dim=1),
+                 theta[:, 1].unsqueeze(dim=1)] * 2
+                , dim=0)  # Shape: 16 * B * 1 * H * W
+            grad_flow_u = kernel * image * grad_bilinearcoef_flow_u  # Shape: 16, B, Ch, H, W
 
             # Get gradient of image pixel value wrt flow: u-direction
-            grad_bilinearcoef_flow_v = torch.stack([-1 + theta_u, -1 + theta_u, 1 - theta_u, 1 - theta_u] * 2 +
-                                                   [-theta_u, -theta_u, theta_u, theta_u] * 2
-                                                   , dim=0)  # Shape: 16 * B * 1 * H * W
-            grad_flow_v = kernel * image_picked_16 * grad_bilinearcoef_flow_v  # Shape: 16, B, Ch, H, W
+            grad_bilinearcoef_flow_v = torch.stack(
+                [-1 + theta[:, 0].unsqueeze(dim=1), -1 + theta[:, 0].unsqueeze(dim=1), 1 - theta[:, 0].unsqueeze(dim=1),
+                 1 - theta[:, 0].unsqueeze(dim=1)] * 2 +
+                [-theta[:, 0].unsqueeze(dim=1), -theta[:, 0].unsqueeze(dim=1), theta[:, 0].unsqueeze(dim=1),
+                 theta[:, 0].unsqueeze(dim=1)] * 2
+                , dim=0)  # Shape: 16 * B * 1 * H * W
+            grad_flow_v = kernel * image * grad_bilinearcoef_flow_v  # Shape: 16, B, Ch, H, W
 
             # Now, let's calculate gradients for Loss
             grad_flow = torch.sum(torch.stack([grad_flow_u, grad_flow_v], dim=0), dim=1) * grad_image_warped.unsqueeze(
